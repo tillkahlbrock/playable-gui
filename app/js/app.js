6adfa47637203d76ed6bd2a231f3d0a846624e9f
Till Kahlbrock <till.kahlbrock@gmail.com>
2014-03-24 13:16:59 +0100

    enables cors

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,11 @@
 'use strict';
 
-angular.module('Playable', ['ngRoute', 'Playable.controllers', 'Playable.services']);
+var playable = angular.module('Playable', ['ngRoute', 'Playable.controllers', 'Playable.services']);
+
+playable.config(function ($httpProvider) {
+    //Enable cross domain calls
+    $httpProvider.defaults.useXDomain = true;
+
+    //Remove the header used to identify ajax call  that would prevent CORS from working
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,15 +2,7 @@
 
 var playableControllers = angular.module('Playable.controllers', []);
 
-playableControllers.controller('PlaygroundCtrl', function($scope, Playground) {
-        $scope.playgrounds = Playground.query();
-
-//    $scope.playgrounds = [
-//        {"name": 'Spielplatz Schlosspark', "latitude": "233", "longitude": "34"},
-//        {"name": "Blub", "latitude": "12", "longitude": "4"},
-//        {"name": "Smup", "latitude": "3", "longitude": "66"},
-//        {"name": "fdsf", "latitude": "33", "longitude": "545"}
-//    ];
-
+playableControllers.controller('PlaygroundCtrl', function ($scope, Playground) {
+    $scope.playgrounds = Playground.query();
     $scope.orderProp = 'latitude'
 });
